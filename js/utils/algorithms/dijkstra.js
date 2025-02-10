76a45a69997f83e1240d6f0091d2ceee88c85c51
EnzoBrancati <enzobrancati28@gmail.com>
2025-02-10 23:12:49 +0100

    allowed speed changes during algorithm execution

--- js/utils/algorithms/dijkstra.js
+++ js/utils/algorithms/dijkstra.js
@@ -3,46 +3,59 @@ import { getNeighbors } from "../pathfinding.js";
 import { speed } from "../../state/state.js";
 
 export function dijkstra(grid, start, end) {
+
     const openSet = [grid[start.y][start.x]];
     const closedSet = [];
+    let delay = speed;
 
     grid[start.y][start.x].g = 0;
 
-    let explorationInterval = setInterval(() => {
-        if (openSet.length === 0) {
-            clearInterval(explorationInterval);
-            return;
-        }
+    function startExplorationInterval() {
+        let explorationInterval = setInterval(() => {
 
-        let current = openSet.reduce((prev, curr) => (prev.g < curr.g ? prev : curr));
-
-        current.element.classList.add("explored");
-
-        if (current.x === end.x && current.y === end.y) {
-            let path = [];
-            let temp = current;
-            while (temp) {
-                path.push(temp);
-                temp = temp.parent;
+            if (delay !== speed) {
+                clearInterval(explorationInterval);
+                delay = speed;
+                startExplorationInterval();
+                return;
             }
-            path.reverse();
-            clearInterval(explorationInterval);
-            visualizePath(path);
-            return;
-        }
+            if (openSet.length === 0) {
+                clearInterval(explorationInterval);
+                return;
+            }
 
-        openSet.splice(openSet.indexOf(current), 1);
-        closedSet.push(current);
+            let current = openSet.reduce((prev, curr) => (prev.g < curr.g ? prev : curr));
 
-        for (let neighbor of getNeighbors(current, grid.length, grid)) {
-            if (!closedSet.includes(neighbor)) {
-                const tentativeG = current.g + 1;
-                if (tentativeG < neighbor.g) {
-                    neighbor.parent = current;
-                    neighbor.g = tentativeG;
-                    if (!openSet.includes(neighbor)) openSet.push(neighbor);
+            current.element.classList.add("explored");
+
+            if (current.x === end.x && current.y === end.y) {
+                let path = [];
+                let temp = current;
+                while (temp) {
+                    path.push(temp);
+                    temp = temp.parent;
+                }
+                path.reverse();
+                clearInterval(explorationInterval);
+                visualizePath(path);
+                return;
+            }
+
+            openSet.splice(openSet.indexOf(current), 1);
+            closedSet.push(current);
+
+            for (let neighbor of getNeighbors(current, grid.length, grid)) {
+                if (!closedSet.includes(neighbor)) {
+                    const tentativeG = current.g + 1;
+                    if (tentativeG < neighbor.g) {
+                        neighbor.parent = current;
+                        neighbor.g = tentativeG;
+                        if (!openSet.includes(neighbor)) openSet.push(neighbor);
+                    }
                 }
             }
-        }
-    }, speed);
+        }, delay);
+    }
+
+    startExplorationInterval();
 }
